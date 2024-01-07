edb1a74dfa6e4886af058eb6eb0d79db20f37ddd
Abhay Kumar Srivastava <ms2364911@gmail.com>
2024-01-07 13:58:12 +0530

    database eidted

--- backend/database.js/database.js
+++ backend/database.js/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 
 const dataBaseConnect = ()=>{
-    mongoose.connect(process.env.URL).then((host)=>{
+    mongoose.connect(process.env.DATABASE_URL).then((host)=>{
         console.log(`mongodb is connected with server data ${host.connection.host}`)
     })
 }
